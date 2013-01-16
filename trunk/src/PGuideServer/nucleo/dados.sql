f0ad2af7d8d31dedc6ed1d8e472b587a0e7aceb8
alezyoliveiraxD@gmail.com <alezyoliveiraxD@gmail.com@e74f74e8-933b-ddb9-8129-f5f450800637>
2013-01-16 01:35:30 +0000

    debugando as geocoordenadas

--- trunk/src/PGuideServer/nucleo/dados.sql
+++ trunk/src/PGuideServer/nucleo/dados.sql
@@ -60,23 +60,41 @@ INSERT INTO nucleo_usuario (user_ptr_id, cidade, estado, preferencias) VALUES (2
 
 INSERT INTO nucleo_marca (id, nome) VALUES (1, "All alimentos");
 INSERT INTO nucleo_marca (id, nome) VALUES (2, "Bic");
+INSERT INTO nucleo_marca (id, nome) VALUES (3, "Nobre");
+INSERT INTO nucleo_marca (id, nome) VALUES (4, "Bauducco");
 
-INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (1, "Mint Strips", "6928544904377", 1, 1, "16", 7);
-INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (2, "Pilhas AAA R03 1,5V Super", "7033080198", 2, 12, "4", 7);
+INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (1, "Mint Strips", "6928544904377", 1, 1, "16", 6);
+INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (2, "Pilhas AAA R03 1,5V Super", "7033080198", 2, 12, "4", 6);
+INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (3, "Álcool Etílico Hidratado", "7896547500102", 3, 25, "500", 8);
+INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (4, "Cookies Original", "7891962026756", 4, 1, "110", 0);
+INSERT INTO nucleo_item (id, nome, codigo, marca_id, categoria_id, tamanho, unidade_id) VALUES (5, "Cookies Chocco", "7891962035505", 4, 1, "105", 0);
 
 INSERT INTO nucleo_itemlista (id, item_id, user_id, quantidade, status) VALUES (1, 1, 2, 2, 1);
 
 INSERT INTO nucleo_reputacao (id, quantidade_avaliacoes, media) VALUES (1, 2, 5.0);
 INSERT INTO nucleo_reputacao (id, quantidade_avaliacoes, media) VALUES (2, 2, 3.4);
 INSERT INTO nucleo_reputacao (id, quantidade_avaliacoes, media) VALUES (3, 2, 4.1);
+INSERT INTO nucleo_reputacao (id, quantidade_avaliacoes, media) VALUES (4, 2, 4.6);
+INSERT INTO nucleo_reputacao (id, quantidade_avaliacoes, media) VALUES (5, 2, 4.0);
+INSERT INTO nucleo_reputacao (id, quantidade_avaliacoes, media) VALUES (6, 2, 3.5);
 
 INSERT INTO nucleo_estabelecimento (id, nome_curto, nome_completo, endereco, bairro, cidade, estado, cnpj, formas_de_pagamento, reputacao_id, latitude, longitude) VALUES (1, "Ponto Frio", "NOVA PONTOCOM COMERCIO ELETRONICO", "Estrada do Ingai, 200", "Dos Altos", "Barueri", "SP", "09.358.108/0002-06", "[1]", 1, -23.498022, -46.967629);
 INSERT INTO nucleo_estabelecimento (id, nome_curto, nome_completo, endereco, bairro, cidade, estado, cnpj, formas_de_pagamento, reputacao_id, latitude, longitude) VALUES (2, "Compra Fácil", "SOC COM IMP HERMES S/A", "Av. Brasil, 44228", "Campo Grande", "Rio de Janeiro", "RJ", "33.068.883/0002-01", "[1]", 2, -22.865687, -43.580608);
 INSERT INTO nucleo_estabelecimento (id, nome_curto, nome_completo, endereco, bairro, cidade, estado, cnpj, formas_de_pagamento, reputacao_id, latitude, longitude) VALUES (3, "Americanas", "B2W COMPANHIA GLOBAL DO VAREJO", "Rod BR 101 Sul, km 29.6, 29600", "Pt dos Carvalho", "Cabo de Santo Agostinho", "PE", "00.776.574/0011-28", "[1]", 3, -8.282078, -35.057817);
+INSERT INTO nucleo_estabelecimento (id, nome_curto, nome_completo, endereco, bairro, cidade, estado, cnpj, formas_de_pagamento, reputacao_id, latitude, longitude) VALUES (4, "Unicompra", "UNICOMPRA SUPERMERCADOS LTDA", "Avenida América, 2960", "Centro", "Palmeira dos Índios", "AL", "78.910.111/0001-28", "[1,2,3,5,8]", 4, -9.408234, -36.631316);
+INSERT INTO nucleo_estabelecimento (id, nome_curto, nome_completo, endereco, bairro, cidade, estado, cnpj, formas_de_pagamento, reputacao_id, latitude, longitude) VALUES (5, "Todo Dia", "BOMPREÇO SUPERMERCADOS DO NORDESTE", "Rua São Braz, 51", "Centro", "Palmeira dos Índios", "AL", "00.776.574/0011-17", "[1,3,4,5,6,7,8]", 5, -9.409637, -36.630286);
+INSERT INTO nucleo_estabelecimento (id, nome_curto, nome_completo, endereco, bairro, cidade, estado, cnpj, formas_de_pagamento, reputacao_id, latitude, longitude) VALUES (6, "Mercadinho Vieira", "JAMES VIEIRA ME", "Rua Tertuliano Canuto, 108", "São Francisco", "Palmeira dos Índios", "AL", "45.012.345/0001-10", "[1]", 6, -9.414612, -36.622402);
 
 INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (1, 1, 1, 1, 2.50, 0, "2012-06-24 12:41:31");
 INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (2, 2, 1, 1, 2.00, 0, "2012-06-24 12:42:31");
 INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (3, 3, 1, 1, 3.00, 50, "2012-06-24 12:42:32");
 INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (4, 1, 2, 1, 2.25, 15, "2012-06-24 12:46:31");
-INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (5, 2, 2, 1, 2.10, 5, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (5, 2, 2, 1, 2.10, 0, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (6, 4, 3, 1, 1.15, 0, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (7, 5, 3, 1, 1.32, 0, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (8, 6, 3, 1, 1.51, 10, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (9, 5, 4, 1, 2.49, 0, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (10, 6, 4, 1, 2.39, 0, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (11, 5, 5, 1, 2.20, 0, "2012-06-24 12:49:31");
+INSERT INTO nucleo_itemestabelecimento (id, estabelecimento_id, item_id, disponibilidade, preco, desconto, data) VALUES (12, 6, 5, 1, 2.25, 5, "2012-06-24 12:49:31");
 
